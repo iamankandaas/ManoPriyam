4124e9c894f0bada06c56cdcff8f85b9b2f25fbf
iamankandaas <das.99.ankan@gmail.com>
2025-08-24 00:10:13 +0530

    FIX: Restore essential config file and correct import

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from config import Config
+from .config import Config
 from zoneinfo import ZoneInfo
 
 # Extensions
